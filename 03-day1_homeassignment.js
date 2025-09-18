0039183fcbef68ff12efc16f6febf8ce62d434c3
reachmejeevak07-cmd <reachmejeevak07@gmail.com>
2025-09-18 18:48:24 +0530

    Add files via upload
    Day 1 
    Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`, 
    including an uninitialized variable.

---
+++ 03-day1_homeassignment.js
@@ -0,0 +1,19 @@
+// a) Declare variable firstName
+let firstName = "John";   // String type
+console.log("Value of firstName:", firstName);
+console.log("Type of firstName:", typeof firstName);
+
+// b) Declare variable companyName
+let companyName;   // Uninitialized variable
+console.log("Value of companyName:", companyName);
+console.log("Type of companyName:", typeof companyName);
+
+// c) Declare variable mobileNumber
+let mobileNumber = 9876543210;   // Number type
+console.log("Value of mobileNumber:", mobileNumber);
+console.log("Type of mobileNumber:", typeof mobileNumber);
+
+// d) Declare variable boolean
+let verifyAccount = true;   // Number type
+console.log("Value of verifyAccount:", verifyAccount);
+console.log("Type of verifyAccount:", typeof verifyAccount);
