58d803b725053c8da7f52c1fa6df12a9add428f5
reachmejeevak07-cmd <reachmejeevak07@gmail.com>
2025-09-20 16:02:33 +0530

    Add files via upload

---
+++ 05-oddoreven.js
@@ -0,0 +1,30 @@
+// Program to find even number
+
+let number = 8; 
+function isoddoreven(number) {
+    if (number % 2 === 0)
+    {
+        console.log("Even number"); 
+        
+    }
+    else {
+        console.log("odd number");  
+    }
+}
+isoddoreven(number)
+
+
+// Program to find odd number
+
+let num = 9;
+function isoddoreven(num) {
+    if (num % 2 === 0)
+    {
+        console.log("Even number"); 
+        
+    }
+    else {
+        console.log("odd number");  
+    }
+}
+isoddoreven(num)
