325af9cf7c649aff1716e0ac35f976c6faa11d0e
reachmejeevak07-cmd <reachmejeevak07@gmail.com>
2025-09-27 15:34:48 +0530

    Add files via upload

---
+++ 03-reverseastring.js
@@ -0,0 +1,9 @@
+let companyName= "testleaf"
+let reversedName="";
+
+for(let i=companyName.length-1; i>=0; i--)
+    {
+    reversedName +=companyName[i];
+}
+console.log("Reversed string is:", reversedName);
+
