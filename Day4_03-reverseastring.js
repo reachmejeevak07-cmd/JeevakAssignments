41e95ba9056db4081183bff0b0a7b9bc7897319f
reachmejeevak07-cmd <reachmejeevak07@gmail.com>
2025-09-27 15:55:42 +0530

    Delete Day4_03-reverseastring.js

--- Day4_03-reverseastring.js
+++
@@ -1,9 +0,0 @@
-let companyName= "testleaf"
-let reversedName="";
-
-for(let i=companyName.length-1; i>=0; i--)
-    {
-    reversedName +=companyName[i];
-}
-console.log("Reversed string is:", reversedName);
-
