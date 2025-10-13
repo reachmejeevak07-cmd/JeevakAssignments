027cb8f2485c817b4c6478716a3541f83abb1c63
reachmejeevak07-cmd <reachmejeevak07@gmail.com>
2025-10-13 21:45:16 +0530

    Update union.ts

--- Day12/union.ts
+++ Day12/union.ts
@@ -1,3 +1,11 @@
+// breakout session : 
+// Classroom activity on union type :
+//  ------------------------------------
+// - Create a function that accepts only specific payment methods.
+// - Create a type alias called PaymentMethod that allows only "UPI", "CreditCard", or "PayPal".
+// - Write a function makePayment that takes a parameter of type PaymentMethod and prints which payment method is chosen.
+// - Call the function with "UPI" and "CreditCard" as arguments.
+
 type PaymentMethod = "UPI" | "CreditCard" | "PayPal" 
 
 
@@ -10,4 +18,5 @@ function makePayment(paymentname:PaymentMethod){
 }
 makePayment("UPI")
 makePayment("CreditCard")
+
 
